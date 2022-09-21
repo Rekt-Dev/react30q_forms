c7fb0e5029a36496191dc22ae368af44b1bd9af9
rektDev <rektdevvv@gmail.com>
2022-09-21 15:58:54 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,46 @@
 import "./styles.css";
-
+import React from "react"
 export default function App() {
   return (
     <div className="App">
       <h1>Learning React forms</h1>
       <form>
+          
           <input
            type="text" 
           name="name"
           placeholder="Whats your name?"
           />
           
+          <input
+           type="date" 
+          name="date of birth"
+          placeholder="Whats your birthday?"
+          />
           
-    
+          <input
+           type="number" 
+          name="fav number"
+          placeholder="Whats your fav number?"
+          />
+          
+          <input
+           type="text" 
+          name="name"
+          placeholder="Whats your pets name?"
+          />
+          
+          <button  
+          
+          type="submit"
+          name="submit">       
+           Submit  
+
+          
+         
+                    
+</button>    
+
           </form>
      
       
